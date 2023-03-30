5a3eb13895f5f5e8c873da9f444f705b6ccc4693
Robert Safin <robertsafin98@gmail.com>
2023-03-30 10:52:21 +0800

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <h2>January 11 2022</h2>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Bootcoomp</h2>
-        <div className="expense-item__price">$7000</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
