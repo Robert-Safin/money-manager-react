193f568804c9945320608c3a57ce1fbdbfbafe7c
Robert Safin <robertsafin98@gmail.com>
2023-03-30 12:15:19 +0800

    custom wrapper

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -7,7 +7,7 @@ function ExpenseDate(props) {
   const year = props.date.getFullYear()
 
   return (
-    <div classname='expense-date'>
+    <div className='expense-date'>
       <div classname='expense-date__month'>{month}</div>
       <div className='expense-date__day'>{day}</div>
       <div className='expense-date__year'>{year}</div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,13 +3,14 @@ import './ExpenseDate'
 import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
+  //title amount date
 
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpenseDate date={props.expense.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{props.expense.title}</h2>
+        <div className="expense-item__price">${props.expense.amount}</div>
       </div>
     </div>
   );
