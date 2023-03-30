34897e79fe01a835da9b37069caa0866bd1ef676
Robert Safin <robertsafin98@gmail.com>
2023-03-30 12:39:22 +0800

    file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
-import Card from "./components/Card";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Card from "./components/UI/Card";
 
 function App() {
   const expenses = [
